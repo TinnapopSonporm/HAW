c692389e437481b57564c2cda6eb83281664cba3
zoidpi <Rusty@Tinnapops-Mac-mini.local>
2015-08-25 00:49:56 +0700

    Slot Rotater  Added
    Clean up SlotTextureGenerator.

---
+++ HAP/HAWPROJECT/Assets/Scripts/SlotRotater.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Slot
+{
+	public class SlotRotater : MonoBehaviour 
+	{
+		private Material[] _slotMaterial;
+		private bool[] _isRunning;
+		private float[] _slotPos = new float[3]{0,0,0};
+		private float _slotSpeed = 0.5f;
+		private int _currentSlotIndex = 0;
+
+		public SlotRotater()
+		{
+			_slotMaterial = new Material[3];
+			_isRunning = new bool[_slotMaterial.Length];
+		}
+
+		void Start () 
+		{
+			for (int i = 0;i < 3;i++)
+			{
+				_slotMaterial[i] = GameObject.Find("SLOT" + (i + 1).ToString()).GetComponent<MeshRenderer>().materials[0];
+			}
+			for (int i = 0; i < _slotMaterial.Length;i++)
+			{
+				_slotMaterial[i].SetTextureOffset("_MainTex", new Vector2(0, 0.2f * i));
+			}
+
+		}
+		void Update () 
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				if (!_isRunning[0] && !_isRunning[1] && !_isRunning[2])
+				{
+					_isRunning[0] = true;
+					_isRunning[1] = true;
+					_isRunning[2] = true;
+					_currentSlotIndex = 0;
+				}
+				else
+				{
+					_isRunning[_currentSlotIndex] = false;
+					_currentSlotIndex++;
+				}
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				if (_isRunning[i])
+				{
+					if (i%2 == 0)
+					{
+						_slotPos[i] += Time.deltaTime * _slotSpeed;
+					}
+					else
+					{
+						_slotPos[i] -= Time.deltaTime * _slotSpeed;
+					}
+					_slotMaterial[i].SetTextureOffset("_MainTex", new Vector2(0, _slotPos[i]));
+				}
+			}
+		}
+	}
+}
--- HAP/HAWPROJECT/Assets/Scripts/SlotTextureGenerator.cs
+++ HAP/HAWPROJECT/Assets/Scripts/SlotTextureGenerator.cs
@@ -6,29 +6,34 @@ namespace Slot
 {
 	public class SlotTextureGenerator : MonoBehaviour 
 	{
-
 		private Texture2D _symbolTexture;
 		public Texture2D[] _slotType;
 		private const int SYMBOL_SIZE = 128;
 		private const int SLOT_SLOT = 3;
-
+		private GameObject[] _slotObject;
 		//hack Slot Assign
 		private List<int[]> slot = new List<int[]>();
 		private int[] slot1 = new int[15]{0, 0, 0, 1, 1, 1, 2, 2, 2, 1, 1, 2, 2, 0, 0};
 		private int[] slot2 = new int[15]{0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 0, 2, 2, 1, 1};
 		private int[] slot3 = new int[15]{0, 0, 0, 1, 1, 1, 2, 2, 2, 1, 2, 0, 0, 2, 1};
+		private Texture2D[] testTex;
 
 		public SlotTextureGenerator()
 		{
 			_slotType = new Texture2D[3];
+			_slotObject = new GameObject[3];
 		}
 
 		void Start()
 		{
-			//Load Symbol Texture
 			_symbolTexture = Resources.Load("CoreSymbol") as Texture2D;
+			for (int i = 0;i < SLOT_SLOT;i++)
+			{
+				_slotObject[i] = GameObject.Find("SLOT" + (i + 1).ToString());
+			}
 			SlotDefine();
 			SetupSlotTexture();
+			AssignTextureToSlot();
 		}
 
 		void SlotDefine()
@@ -40,8 +45,6 @@ namespace Slot
 
 		void SetupSlotTexture()
 		{
-
-			// Split Symbol one by one
 			for (int i = 0; i < _slotType.Length; i++)
 			{
 				Color[] tempColor = _symbolTexture.GetPixels(SYMBOL_SIZE * i, 0, SYMBOL_SIZE, SYMBOL_SIZE);
@@ -49,8 +52,6 @@ namespace Slot
 				_slotType[i].SetPixels(tempColor);
 				_slotType[i].Apply();
 			}
-			//
-
 			Texture2D[] tSlot = new Texture2D[SLOT_SLOT];
 			for (int i = 0; i < tSlot.Length; i++)
 			{
@@ -63,22 +64,14 @@ namespace Slot
 			}
 			testTex = tSlot;
 		}
-
-		void Update () {
-			
-		}
-
-		//Test Only
-		public Texture2D[] testTex;
-		void OnGUI()
+		void AssignTextureToSlot()
 		{
-			GUI.DrawTexture(new Rect(0,0,testTex[0].width/4, testTex[0].height/4),testTex[0], ScaleMode.ScaleToFit);
-			GUI.DrawTexture(new Rect(100,0,testTex[1].width/4, testTex[1].height/4),testTex[1], ScaleMode.ScaleToFit);
-			GUI.DrawTexture(new Rect(200,0,testTex[2].width/4, testTex[2].height/4),testTex[2], ScaleMode.ScaleToFit);
+			for (int i = 0; i < _slotObject.Length;i++)
+			{
+				_slotObject[i].GetComponent<MeshRenderer>().materials[0].mainTexture = testTex[i];
+				_slotObject[i].GetComponent<MeshRenderer>().materials[0].SetTextureScale("_MainTex" ,new Vector2(1f, 0.2f));
+			}
 		}
-
-			
-			
 	}
 	public enum SlotType
 	{
